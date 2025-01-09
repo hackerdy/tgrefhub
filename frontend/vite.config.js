c169adbe62656a81c97a3d26d31d530b5c45ee36
DESTINY <moregiedestiny2@gmail.com>
2025-01-09 18:43:24 +0100

    output dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Explicitly set the output directory
+  },
   define: {
     'process.env': {}, // Prevent `process` undefined error
   },
