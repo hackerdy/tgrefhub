bad26581d0663145be0ded3ba4dcd0161ba9737e
DESTINY <moregiedestiny2@gmail.com>
2025-01-11 08:57:48 +0100

    Trigger new deployment

--- frontend/src/AppWrapper.jsx
+++ frontend/src/AppWrapper.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
+// Removed dotenv import and config as it's handled by dotenv-webpack
+import { TwaAnalyticsProvider } from '@tonsolutions/telemetree-react';
 import UserContext from './utils/UserContext';
 import App from './App.jsx';
 
-
 export default function AppWrapper() {
-  const [userData, setUserData] = useState(null); // useState to manage user data
+  const [userData, setUserData] = useState(null);
 
   return (
-    <UserContext.Provider value={{ userData, setUserData }}>
-
-      <App /> {/* App is now a child of the provider */}
-    </UserContext.Provider>
+    <TwaAnalyticsProvider 
+      projectId={import.meta.env.VITE_PROJECT_ID}
+      apiKey={import.meta.env.VITE_API_KEY}
+      appName="Telegram Referral Hub"
+    >
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <App />
+      </UserContext.Provider>
+    </TwaAnalyticsProvider>
   );
 }
